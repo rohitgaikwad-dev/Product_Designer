99263e80636bfaf7e6f1af7c16635ff63a4ec7a8
Rohit Gaikwad <Rohitgaikwad78963@gmail.com>
2023-09-12 22:48:19 +0530

    mouse follower glitch update

--- js/main.js
+++ js/main.js
@@ -1,25 +1,68 @@
+let timeout;
+
 const scroll = new LocomotiveScroll({
   el: document.querySelector("#main"),
   smooth: true,
 });
 
-let circleMouseFollower = () => {
-  const cricle = document.querySelector("#circle");
+let homeAnime = () => {
+  let tl = gsap.timeline();
 
+  tl.from("#nav", {
+    y: -25,
+    opacity: 0,
+    duration: 1.5,
+    ease: Expo.easeInOut,
+  })
+    .to(".boundingelem", {
+      y: 0,
+      duration: 2,
+      ease: Expo.easeInOut,
+      delay: -1,
+      stagger: 0.2,
+    })
+    .from("#home_footer", {
+      y: 10,
+      opacity: 0,
+      duration: 1.5,
+      delay: -1,
+      ease: Expo.easeInOut,
+    });
+};
+
+let circleScale = () => {
+  const circle = document.querySelector("#circle");
+
+  let xscale = 1;
+  let yscale = 1;
+
+  let xprev = 0;
+  let yprev = 0;
   window.addEventListener("mousemove", (details) => {
-    let x = details.pageX;
-    let y = details.pageY;
+    clearTimeout(timeout);
 
-    cricle.style.transform = `translateX(${x}px) translateY(${y}px)`;
-  });
+    xscale = gsap.utils.clamp(0.8, 1.2, details.pageX - xprev);
+    yscale = gsap.utils.clamp(0.8, 1.2, details.pageY - yprev);
 
-  window.addEventListener("mouseout", (details) => {
-    if (details.pageY <= 0 || details.pageX >= innerWidth) {
-      cricle.style.opacity = "0";
-    } else {
-      cricle.style.opacity = "1";
-    }
+    xprev = details.pageX;
+    yprev = details.pageY;
+
+    circleMouseFollower(xscale, yscale);
+
+    timeout = setTimeout(() => {
+      circle.style.transform = `translateX(${details.pageX}px) translateY(${details.pageY}px) scale(1, 1)`;
+    }, 100);
   });
 };
 
+let circleMouseFollower = (xscale, yscale) => {
+  const circle = document.querySelector("#circle");
+
+  window.addEventListener("mousemove", (details) => {
+    circle.style.transform = `translate(${details.pageX}px, ${details.pageY}px) scale(${xscale}, ${yscale})`;
+  });
+};
+
+homeAnime();
+circleScale();
 circleMouseFollower();
