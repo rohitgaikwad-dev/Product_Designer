98338780e4b9523e0c955adcba768b8f2d2919f1
Rohit Gaikwad <61135868+rohitgaikwad-dev@users.noreply.github.com>
2024-01-24 21:58:33 +0530

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,3 +1,10 @@
+let cursor = new MouseFollower();
+
+const scroll = new LocomotiveScroll({
+  el: document.querySelector("#main"),
+  smooth: true,
+});
+
 const startPreloader = () => {
   const preloader = document.querySelector("#preloader");
   const barconfirm = document.querySelector("#barconfirm");
@@ -5,99 +12,94 @@ const startPreloader = () => {
   const main = document.querySelector("#main");
 
   let width = 1;
+  let id;
 
   const frame = () => {
     if (width >= 100) {
+      clearInterval(id);
       tl.play();
     } else {
       width++;
-      barconfirm.style.width = `${width}%`;
-      percent.innerHTML = `${width}%`;
-      requestAnimationFrame(frame);
+      barconfirm.style.width = width + "%";
+      percent.innerHTML = width + "%";
     }
   };
 
-  frame();
+  id = setInterval(frame, 10);
 
+  // Timeline for preloader animation
   const tl = gsap.timeline({
     paused: true,
   });
 
   tl.to("#percent, #bar", {
-      duration: 0.4,
-      opacity: 0,
-      zIndex: -1,
-    }, 'preloader')
+    duration: 0.4,
+    opacity: 0,
+    zIndex: -1,
+  })
     .to("#preloader", {
       duration: 0.8,
       height: "0%",
-    }, 'preloader')
+    },'a')
     .from("#main", {
       duration: 1.4,
       y: "150%",
       delay: -0.6,
-    }, 'preloader')
+    },'a')
     .to("#main", {
       opacity: 1,
       y: "0%",
-    })
-    .from("#nav", {
+    }).from("#nav", {
       y: -25,
       opacity: 0,
       duration: 1.5,
-    })
-    .to(".boundingelem", {
-      y: 0,
-      duration: 2,
-      stagger: 0.2,
-    }, '-=1') // Adjusted delay
-    .from("#home_footer", {
-      y: 10,
-      opacity: 0,
-      duration: 1.5,
       ease: Expo.easeInOut,
-    }, '-=1'); // Adjusted delay
+    })
+      .to(".boundingelem", {
+        y: 0,
+        duration: 2,
+        ease: Expo.easeInOut,
+        delay: -1,
+        stagger: 0.2,
+      })
+      .from("#home_footer", {
+        y: 10,
+        opacity: 0,
+        duration: 1.5,
+        delay: -1,
+        ease: Expo.easeInOut,
+      });
 };
-
-const handleMouseMove = (elem, e) => {
-  const { top } = elem.getBoundingClientRect();
-  const diffY = e.pageY - top;
-
-  gsap.to(elem.querySelector("img"), {
-    opacity: 1,
-    ease: Power3,
-    top: diffY,
-    left: e.pageX,
-    rotate: gsap.utils.clamp(-20, 20, rotateDiff(e)),
-  });
-};
-
-const handleMouseLeave = (elem) => {
-  gsap.to(elem.querySelector("img"), {
-    opacity: 0,
-    ease: Power3,
-  });
-};
-
-const rotateDiff = (e) => {
-  const diffRot = rotate - e.pageX;
-  rotate = e.pageX;
-  return diffRot * 0.5;
-};
-
-let rotate = 0;
+window.addEventListener("load", startPreloader());
 
 const showImage = () => {
   document.querySelectorAll(".elem").forEach((elem) => {
-    elem.addEventListener("mousemove", (e) => handleMouseMove(elem, e));
+    let rotate = 0;
+    let diffrot = 0;
+    elem.addEventListener("mousemove", (e) => {
+      let diffY = e.pageY - elem.getBoundingClientRect().top;
+      diffrot = rotate - e.pageX;
+      rotate = e.pageX;
+
+      gsap.to(elem.querySelector("img"), {
+        opacity: 1,
+        ease: Power3,
+        top: diffY,
+        left: e.pageX,
+        rotate: gsap.utils.clamp(-20, 20, diffrot * 0.5),
+      });
+    });
   });
 };
-
 const hideImage = () => {
   document.querySelectorAll(".elem").forEach((elem) => {
-    elem.addEventListener("mouseleave", () => handleMouseLeave(elem));
+    elem.addEventListener("mouseleave", (e) => {
+      gsap.to(elem.querySelector("img"), {
+        opacity: 0,
+        ease: Power3,
+      });
+    });
   });
 };
-
 showImage();
 hideImage();
